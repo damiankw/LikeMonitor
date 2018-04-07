2a3bb403780259e2623695819c0f3f764cbfc7ac
damian <damian@damian.id.au>
2018-04-07 17:02:21 +1000

    Added configuration file.

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+  $_CONFIG = array(
+    'refresh' => 60,                 // refresh rate in seconds
+    'logo' => 'http://www.mywebsite.com/logo.png', // link to the logo (local or remote)
+    'facebook' => 'MyFacebookPage',  // facebook page
+    'twitter' => 'MyTwitterPage',    // twitter page
+    'instagram' => 'MyInstagramPage' // instagram page
+  );
+?>
