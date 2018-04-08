68331a6924c0d2732ff2f55b14108feb5f277c4e
damian <damian@damian.id.au>
2018-04-08 10:56:41 +1000

    Added actual config.

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+  $_CONFIG = array(
+    'title' => 'Gameroom Essentials', // title of the page
+    'logo' => 'http://www.gameroom.com.au/images/logo2.png', // link to the logo (local or remote)
+    'refresh' => 5,                                         // refresh rate in seconds
+    'facebook' => 'GameRoomEssentials',                          // facebook page
+    'twitter' => 'gameroom_au',                            // twitter page
+    'instagram' => 'gameroom_au'                         // instagram page
+  );
+?>
