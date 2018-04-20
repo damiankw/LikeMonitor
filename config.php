0fc5c2fe5006ba49304a2b10705916cf3b0db3a5
Damian West <damian@damian.id.au>
2018-04-20 15:54:36 +1000

    Added logo url into config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?php
   $_CONFIG = array(
     'title' => 'Gameroom Essentials', // title of the page
+    'url' => 'http://www.gameroom.com.au/', // url of the website (if there is one)
     'logo' => 'http://www.gameroom.com.au/images/logo2.png', // link to the logo (local or remote)
     'refresh' => 5,                                         // refresh rate in seconds
     'facebook' => 'GameRoomEssentials',                          // facebook page
