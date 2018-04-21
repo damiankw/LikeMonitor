5a13d249b18e64fc488215d5932dcc0c8ca93d9a
damian <damian@damian.id.au>
2018-04-21 14:21:46 +1000

    Fixed instagram breaking their simple json output. (only for low numbered followers).

--- monitor.php
+++ monitor.php
@@ -71,7 +71,7 @@ function get_facebook($PAGE, $IMG=false) {
 
 function get_instagram($PAGE, $IMG=false) {
   // build the URL for Instagram
-  $URL = 'https://www.instagram.com/' . $PAGE .'/?__a=1';
+  $URL = 'https://www.instagram.com/' . $PAGE;
 
   // set up CURL and execute (we are making out to be a browser here)
   $CURL = curl_init($URL);
@@ -80,13 +80,16 @@ function get_instagram($PAGE, $IMG=false) {
   curl_setopt($CURL, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.7.12) Gecko/20050915 Firefox/1.0.7");
   curl_setopt($CURL, CURLOPT_HEADER, false );
   curl_setopt($CURL, CURLOPT_RETURNTRANSFER, true );
-  $JSON = curl_exec($CURL);
+  $HTML = curl_exec($CURL);
 
-  // turn JSON into array
-  $DATA = json_decode($JSON, true);
+  // check if there is a valid return, if not the page must be invalid (this will be outdated after 1bil followers on a page. i'm not good at regex)
+  if (!preg_match('/content="(\d{1,3}\,\d{1,3}\,\d{1,3}\,\d{1,3}|\d{1,3}\,\d{1,3}\,\d{1,3}|\d{1,3}\,\d{1,3}|\d{1,3}) Followers/', $HTML, $MATCH)) {
+    die('ERROR [0002] PAGE INVALID');
+  }
 
-  // output the required information
-  $TOTAL = sprintf('%06d', $DATA['graphql']['user']['edge_followed_by']['count']);
+  // NOTE: need to add another thing in here to convert 1.68bn to the real number (or find another way)
+  // return the solid number with no commas
+  $TOTAL = sprintf('%06d', str_replace(',', '', $MATCH[1]));
 
   // output whatever is required
   if ($IMG) {
