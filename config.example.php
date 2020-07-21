89e143f494ecd13a272e28571296f9486d2d67f7
damian <damian@damian.id.au>
2020-07-21 10:33:56 +1000

    Updates.

--- config.example.php
+++ config.example.php
@@ -4,6 +4,7 @@
     'url' => 'http://www.mywebsite.com/', // url of the website (if there is one)
     'logo' => 'http://www.mywebsite.com/logo.png', // link to the logo (local or remote)
     'refresh' => 60,                 // refresh rate in seconds
+    'style' => 'standard',           // type of style (slide, standard)
     'facebook' => 'MyFacebookPage',  // facebook page
     'twitter' => 'MyTwitterPage',    // twitter page
     'instagram' => 'MyInstagramPage' // instagram page
--- config.php
+++ config.php
@@ -4,6 +4,7 @@
     'url' => 'http://www.gameroom.com.au/', // url of the website (if there is one)
     'logo' => 'http://www.gameroom.com.au/images/logo2.png', // link to the logo (local or remote)
     'refresh' => 5,                                         // refresh rate in seconds
+    'style' => 'slide',                                         // type of style (slide, standard)
     'facebook' => 'GameRoomEssentials',                          // facebook page
     'twitter' => 'gameroom_au',                            // twitter page
     'instagram' => 'gameroom_au'                         // instagram page
